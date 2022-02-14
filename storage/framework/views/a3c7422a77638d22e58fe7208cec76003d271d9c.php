e2a5466be9343044310041e0b7d5b99216ca50f9
Shah Hussain <shahhussaindev@gmail.com>
2022-02-14 15:52:10 +0530

    revert

--- storage/framework/views/a3c7422a77638d22e58fe7208cec76003d271d9c.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\cws\Documents\Master Laravel Project\Total-Services\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
